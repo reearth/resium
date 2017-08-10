377502b4afd148ab1092f9e245df0361da97cfbb
rot1024 <rot1024@gmail.com>
2017-08-10 14:00:43 +0900

    use karma-rollup-preprocessor instead of babel

--- karma.conf.js
+++ karma.conf.js
@@ -2,16 +2,35 @@
 
 module.exports = function(config) {
   config.set({
+    browsers: ["ChromeHeadless"],
     frameworks: ["jasmine"],
     files: [
+      "node_modules/cesium/Build/CesiumUnminified/Cesium.js",
       "src/**/*.test.js"
     ],
     preprocessors: {
-      "src/**/*.test.js": ["babel"]
+      "src/**/*.test.js": ["rollup"]
     },
     reporters: ["progress"],
     autoWatch: false,
-    browsers: ["ChromeHeadless"],
-    singleRun: true
+    singleRun: true,
+    rollupPreprocessor: {
+      plugins: [
+        require("rollup-plugin-node-resolve")(),
+        require("rollup-plugin-commonjs")(),
+        require("rollup-plugin-babel")({
+          exclude: "node_modules/**"
+        }),
+        require("rollup-plugin-replace")({
+          "process.env.NODE_ENV": JSON.stringify("development")
+        })
+      ],
+      format: "iife",
+      sourceMap: "inline",
+      globals: {
+        cesium: "Cesium"
+      },
+      external: ["cesium"]
+    }
   });
 };
