b4032c1691a26e725e13d0e3655e697e64aadaad
rot1024 <rot1024@gmail.com>
2017-08-11 00:34:49 +0900

    implement entity component simply

---
+++ src/entity.js
@@ -0,0 +1,67 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Entity as CesiumEntity } from "cesium";
+
+import viewerType from "./propTypes/viewer";
+
+export default class Entity extends React.PureComponent {
+
+  static propTypes = {
+    description: PropTypes.string,
+    id: PropTypes.string,
+    name: PropTypes.string,
+    position: PropTypes.object
+  }
+
+  static defaultProps = {
+
+  }
+
+  static contextTypes = {
+    viewer: viewerType
+  }
+
+  componentDidMount() {
+    const {
+      id,
+      name,
+      position,
+      description
+    } = this.props;
+    this.entity = new CesiumEntity({
+      id,
+      name,
+      position,
+      description,
+      point: {
+        pixelSize: 10
+      }
+    });
+    this.context.viewer.entities.add(this.entity);
+  }
+
+  componentDidUpdate(prevProps) {
+    const e = this.entity;
+    const p = this.props;
+    if (process.env.NODE_ENV !== "production" && p.id !== prevProps.id) {
+      console.warn("Cesium entity id prop is not changable.");
+    }
+    e.name = p.name;
+    e.position = p.position;
+    e.description = p.description;
+  }
+
+  componentWillUnmount() {
+    if (this.context.viewer && !this.context.viewer.isDestroyed()) {
+      this.context.viewer.entities.remove(this.entity);
+    }
+    this.entity = null;
+  }
+
+  entity = null;
+
+  render() {
+    return null;
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export * as PropTypes from "./propTypes";
 
 export Viewer from "./viewer";
+export Entity from "./entity";
