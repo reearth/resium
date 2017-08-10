487fefc52a714ab301a93438057cbfcec23adf22
rot1024 <rot1024@gmail.com>
2017-08-11 00:35:30 +0900

    add simple-entity example

--- examples/src/pages/index.js
+++ examples/src/pages/index.js
@@ -1,9 +1,15 @@
 import Basic from "./basic";
+import SimpleEntity from "./simple-entity";
 
 export default [
   {
     name: "Basic",
     slug: "basic",
     component: Basic
+  },
+  {
+    name: "Simple entity",
+    slug: "entity",
+    component: SimpleEntity
   }
 ];
---
+++ examples/src/pages/simple-entity.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Cartesian3 } from "cesium";
+import { Viewer, Entity } from "cesium-react";
+
+const positions = [
+  Cartesian3.fromDegrees(-74.0607383, 40.7117244, 100),
+  Cartesian3.fromDegrees(139.767052, 35.681167, 100)
+];
+
+export default class SimpleEntity extends React.PureComponent {
+
+  state = {
+    pos: 0
+  }
+
+  changePosition(pos) {
+    this.setState({ pos });
+  }
+
+  render() {
+    return (
+      <Viewer full>
+        <Entity
+          name="test"
+          description="test"
+          position={positions[this.state.pos]} />
+        <div
+          style={{
+            position: "absolute",
+            left: "10px",
+            top: "10px",
+            zIndex: "100"
+          }}>
+          <button
+            onClick={() => this.changePosition(0)}>
+            New York
+          </button>
+          <button
+            onClick={() => this.changePosition(1)}>
+            Tokyo
+          </button>
+        </div>
+      </Viewer>
+    );
+  }
+
+}
