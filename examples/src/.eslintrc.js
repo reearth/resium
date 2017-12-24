ea4574307c36020c371cc61b2116f733e4e53f64
rot1024 <aayhrot@gmail.com>
2017-12-24 18:07:29 +0900

    update examples eslint config

---
+++ examples/src/.eslintrc.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  "settings": {
+    "import/resolver": {
+      "webpack": {
+        "config": {
+          resolve: {
+            alias: {
+              cesium: path.resolve(__dirname, "../../node_modules/cesium/Build/Cesium/Cesium"),
+              "cesium-react": path.resolve(__dirname, "../../src")
+            }
+          }
+        }
+      }
+    }
+  },
+  "rules": {
+    "import/no-unresolved": 2,
+    "node/no-missing-import": 0
+  }
+};
