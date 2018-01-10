39470028c598efdbfcbe8ae6164225c0889b62b1
rot1024 <aayhrot@gmail.com>
2018-01-11 01:21:28 +0900

    set entity proptypes

--- src/entity.js
+++ src/entity.js
@@ -8,10 +8,33 @@ export default class Entity extends CesiumComponent {
 
   static propTypes = {
     ...CesiumComponent.propTypes,
-    description: PropTypes.string,
-    id: PropTypes.string,
-    name: PropTypes.string,
-    position: PropTypes.object
+    availability: PropTypes.any,
+    billboard: PropTypes.any,
+    box: PropTypes.any,
+    corridor: PropTypes.any,
+    cylinder: PropTypes.any,
+    description: PropTypes.any,
+    ellipse: PropTypes.any,
+    ellipsoid: PropTypes.any,
+    id: PropTypes.any,
+    label: PropTypes.any,
+    model: PropTypes.any,
+    name: PropTypes.any,
+    onDefinitionChanged: PropTypes.func,
+    orientation: PropTypes.any,
+    parent: PropTypes.any,
+    path: PropTypes.any,
+    plane: PropTypes.any,
+    point: PropTypes.any,
+    polygon: PropTypes.any,
+    polyline: PropTypes.any,
+    polylineVolume: PropTypes.any,
+    position: PropTypes.any,
+    properties: PropTypes.any,
+    rectangle: PropTypes.any,
+    show: PropTypes.any,
+    viewFrom: PropTypes.any,
+    wall: PropTypes.any
   }
 
   static contextTypes = {
