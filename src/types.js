5513e0b75a573f64e195ae38fd5277ebb9f7267d
rot1024 <aayhrot@gmail.com>
2018-01-11 01:28:29 +0900

    add types

--- src/types.js
+++ src/types.js
@@ -1,6 +1,14 @@
-import { Camera, Scene, Viewer } from "cesium";
+import {
+  Camera,
+  EntityCollection,
+  PremitiveCollection,
+  Scene,
+  Viewer
+} from "cesium";
 import PropTypes from "prop-types";
 
 export const cameraType = PropTypes.instanceOf(Camera);
+export const entityCollectionType = PropTypes.instanceOf(EntityCollection);
+export const premitiveCollectionType = PropTypes.instanceOf(PremitiveCollection);
 export const sceneType = PropTypes.instanceOf(Scene);
 export const viewerType = PropTypes.instanceOf(Viewer);
