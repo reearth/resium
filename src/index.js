a400b49a5cf0ed8fe7b6351b66772796740320d7
rot1024 <aayhrot@gmail.com>
2018-01-12 20:21:10 +0900

    expose datasource components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 export * as PropTypes from "./types";
 
+export Viewer from "./Viewer";
+
 export Entity from "./Entity";
+
+export DataSource from "./DataSource";
+export CustomDataSource from "./CustomDataSource";
+export CzmlDataSource from "./CzmlDataSource";
+export GeoJsonDataSource from "./GeoJsonDataSource";
+export KmlDataSource from "./KmlDataSource";
+
+export Primitive from "./Primitive";
 export PointPrimitive from "./PointPrimitive";
 export PointPrimitiveCollection from "./PointPrimitiveCollection";
-export Primitive from "./Primitive";
-export Viewer from "./Viewer";
