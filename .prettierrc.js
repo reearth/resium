ea7418ec3be9adfadb9f37efb2dae5afec60bb70
rot1024 <aayhrot@gmail.com>
2018-02-09 23:00:50 +0900

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid",
+  parser: "babylon",
+  proseWrap: "preserve",
+};
