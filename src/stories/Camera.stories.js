79e8878a847e3b7cae58ea83e48bf216a10000cf
rot1024 <aayhrot@gmail.com>
2018-09-26 17:39:37 +0900

    add camera component example

--- src/stories/Camera.stories.js
+++ src/stories/Camera.stories.js
@@ -4,10 +4,16 @@ import { storiesOf } from "@storybook/react";
 
 import Viewer from "../Viewer";
 import CameraFlyTo from "../CameraFlyTo";
+import Camera from "../Camera";
 
 export default () => {
   storiesOf("Camera", module).addWithJSX("default", () => (
     <Viewer full>
+      <Camera
+        onMoveEnd={e => console.log("end", e)}
+        onMoveStart={e => console.log("start", e)}
+        onChanged={e => console.log("changed", e)}
+      />
       <CameraFlyTo duration={5} destination={Cartesian3.fromDegrees(139.767052, 35.681167, 100)} />
     </Viewer>
   ));
