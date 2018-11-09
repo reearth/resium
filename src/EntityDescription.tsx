c73393a5c880e55e002388dee25a46de19a57559
rot1024 <aayhrot@gmail.com>
2018-11-09 13:27:30 +0900

    add: EntityDescription component

---
+++ src/EntityDescription.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import { ConstantProperty } from "cesium";
+
+import { withContext } from "./core/context";
+import { Entity } from "cesium";
+
+export interface Props {
+  cesium: { entity: Entity };
+}
+
+class EntityDescription extends React.PureComponent<Props> {
+  public constructor(props: Readonly<Props> & { children?: React.ReactNode }) {
+    super(props);
+    this.update(props);
+  }
+
+  public componentDidMount() {
+    this.update();
+  }
+
+  public componentDidUpdate(prevProps: Readonly<Props> & { children?: React.ReactNode }) {
+    if (this.props.children !== prevProps.children) {
+      this.update();
+    }
+  }
+
+  public render() {
+    return null;
+  }
+
+  private update(props: Readonly<Props> & { children?: React.ReactNode } = this.props) {
+    if (props.cesium && props.cesium.entity && props.children) {
+      props.cesium.entity.description = new ConstantProperty(
+        ReactDOMServer.renderToStaticMarkup(props.children as React.ReactElement<any>),
+      );
+    }
+  }
+}
+
+export default withContext(EntityDescription);
