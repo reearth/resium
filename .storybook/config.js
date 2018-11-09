4ffdfe3f716937529dd7e1c74ce4a8a9b753e40b
rot1024 <aayhrot@gmail.com>
2018-11-09 15:45:31 +0900

    refactor: rewrite storybooks in typescript

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from "@storybook/react";
 
-const req = require.context("../src", true, /\.?stories\.js$/);
+const req = require.context("../src", true, /\.?stories\.(?:j|t)sx?$/);
 
 configure(() => {
   req.keys().forEach(filename => req(filename).default());
