e21a5c9bd9724f64bcae70c8d611706c320d6f36
rot1024 <aayhrot@gmail.com>
2018-11-14 16:10:35 +0900

    add: ExtendedPrimitive/PointPrimitive

---
+++ src/ExtendedPrimitive.ts
@@ -0,0 +1,6 @@
+import Primitive from "./Primitive";
+import createEventWrapper from "./core/EventWrapper";
+
+const ExtendedPrimitve = createEventWrapper(Primitive);
+
+export default ExtendedPrimitve;
