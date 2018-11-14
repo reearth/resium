bddf0c4a0e70827ca27bf640f47821e60317b741
rot1024 <aayhrot@gmail.com>
2018-11-14 15:15:03 +0900

    change: reimpl PointPrimive/Collection in ts

---
+++ src/PointPrimitive.ts
@@ -0,0 +1,58 @@
+import Cesium from "cesium";
+
+import createCesiumComponent from "./core/CesiumComponent";
+
+export interface PointPrimitiveCesiumProps {
+  color?: Cesium.Color;
+  disableDepthTestDistance?: boolean;
+  distanceDisplayCondition?: Cesium.DistanceDisplayCondition;
+  id?: any;
+  outlineColor?: Cesium.Color;
+  outlineWidth?: number;
+  pixelSize?: number;
+  position?: Cesium.Cartesian3;
+  scaleByDistance?: Cesium.NearFarScalar;
+  show?: boolean;
+  translucencyByDistance?: Cesium.NearFarScalar;
+}
+
+export interface PointPrimitiveContext {
+  pointPrimitiveCollection: Cesium.PointPrimitiveCollection;
+}
+
+const cesiumProps: Array<keyof PointPrimitiveCesiumProps> = [
+  "color",
+  "disableDepthTestDistance",
+  "distanceDisplayCondition",
+  "id",
+  "outlineColor",
+  "outlineWidth",
+  "pixelSize",
+  "position",
+  "scaleByDistance",
+  "show",
+  "translucencyByDistance",
+];
+
+const PointPrimitive = createCesiumComponent<
+  Cesium.PointPrimitive,
+  PointPrimitiveCesiumProps,
+  PointPrimitiveContext
+>({
+  name: "PointPrimitive",
+  create() {
+    return new Cesium.PointPrimitive();
+  },
+  mount(element, context) {
+    context.pointPrimitiveCollection.add(element);
+  },
+  unmount(element, context) {
+    if (!context.pointPrimitiveCollection.isDestroyed) {
+      context.pointPrimitiveCollection.remove(element);
+    }
+  },
+  cesiumProps,
+  setCesiumPropsAfterCreate: true,
+});
+
+export default PointPrimitive;
