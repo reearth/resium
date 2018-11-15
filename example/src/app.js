36a4db3685f030c8bfdfc9f7866a953b39aae3dc
rot1024 <aayhrot@gmail.com>
2018-11-15 14:19:33 +0900

    fix: fix example

--- example/src/app.js
+++ example/src/app.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 import { Cartesian3 } from "cesium";
-import { Viewer, Entity } from "resium";
+import { Viewer, Entity } from "../../dist/cesium-react.es";
 
 const App = () => (
   <Viewer full>
     <Entity
       name="Tokyo"
       position={Cartesian3.fromDegrees(139.767052, 35.681167, 100)}
-      point={{ pixelSize: 10 }}>
-      <h1>Hello, world.</h1>
-    </Entity>
+      point={{ pixelSize: 10 }}
+      description="hoge"
+    />
   </Viewer>
 );
 
