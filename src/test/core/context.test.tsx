77c952cb2efc50896db9f0fe0c3cf6f83d4dd128
rot1024 <aayhrot@gmail.com>
2018-11-16 16:32:12 +0900

    test: fix test

--- src/test/core/context.test.tsx
+++ src/test/core/context.test.tsx
@@ -6,16 +6,17 @@ import { Provider } from "../../core/context";
 
 describe("core/context", () => {
   it("should inject context to cesium prop", () => {
-    const Dummy: React.SFC<{ cesium: { dummy: string } }> = ({ cesium }) => (
-      <div>{cesium.dummy}</div>
-    );
+    const Dummy: React.SFC<{ cesium: { dummy: string } }> = () => null;
     const WithContextDummy = withContext<{}, { dummy: string }>(Dummy);
 
+    const value = { dummy: "test" };
+
     const wrapper = mount(
-      <Provider value={{ dummy: "test" }}>
+      <Provider value={value}>
         <WithContextDummy />
       </Provider>,
     );
-    expect(wrapper.find(Dummy).prop("cesium")).toEqual({ dummy: "test" });
+
+    expect(wrapper.find(Dummy).prop("cesium")).toBe(value);
   });
 });
