7384b39c10b69b4659e60ee9c76c877c789aa370
rot1024 <aayhrot@gmail.com>
2018-11-16 18:26:28 +0900

    test: fix mock

--- __mocks__/cesium/Event.ts
+++ __mocks__/cesium/Event.ts
@@ -1,21 +1,18 @@
 export default class Event {
-  private events: Set<(...args: any[]) => void> = new Set();
-
-  public addEventListener(e: (...args: any[]) => void, scope: any) {
-    this.events.add(e);
-  }
-
-  public removeEventListener(e: (...args: any[]) => void, scope: any) {
-    this.events.delete(e);
-  }
-
-  public raiseEvent(...args: any[]) {
-    this.events.forEach(e => {
-      e(...args);
-    });
-  }
-
   public get numberOfListeners() {
     return this.events.size;
   }
+
+  public addEventListener: (e: (...args: any[]) => void, scope: any) => void = jest.fn(e => {
+    this.events.add(e);
+  });
+  public removeEventListener: (e: (...args: any[]) => void, scope: any) => void = jest.fn(e => {
+    this.events.delete(e);
+  });
+  public raiseEvent: (...args: any[]) => void = jest.fn(args => {
+    this.events.forEach(e => {
+      e(...args);
+    });
+  });
+  private events: Set<(...args: any[]) => void> = new Set();
 }
