bf16456e2d4946190a3bb7573e2170c97062fcc5
rot1024 <aayhrot@gmail.com>
2018-11-16 18:28:25 +0900

    fix: rename

--- __mocks__/cesium/Event.ts
+++ __mocks__/cesium/Event.ts
@@ -1,18 +1,21 @@
 export default class Event {
-  public get numberOfListeners() {
-    return this.events.size;
+  private events: Set<(...args: any[]) => void> = new Set();
+
+  public addEventListener(e: (...args: any[]) => void, scope: any) {
+    this.events.add(e);
   }
 
-  public addEventListener: (e: (...args: any[]) => void, scope: any) => void = jest.fn(e => {
-    this.events.add(e);
-  });
-  public removeEventListener: (e: (...args: any[]) => void, scope: any) => void = jest.fn(e => {
+  public removeEventListener(e: (...args: any[]) => void, scope: any) {
     this.events.delete(e);
-  });
-  public raiseEvent: (...args: any[]) => void = jest.fn(args => {
+  }
+
+  public raiseEvent(...args: any[]) {
     this.events.forEach(e => {
       e(...args);
     });
-  });
-  private events: Set<(...args: any[]) => void> = new Set();
+  }
+
+  public get numberOfListeners() {
+    return this.events.size;
+  }
 }
