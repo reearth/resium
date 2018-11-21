5223d866b844e7278755ed0e521bd328d2a362b1
rot1024 <aayhrot@gmail.com>
2018-11-21 13:57:37 +0900

    change moon storybook

--- src/stories/Moon.stories.tsx
+++ src/stories/Moon.stories.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import Cesium from "cesium";
 
 import Viewer from "../Viewer";
 import Moon from "../Moon";
 
+const radius = Cesium.Math.LUNAR_RADIUS * 10;
+
 export default () => {
   storiesOf("Moon", module).add("default", () => (
     <Viewer full>
-      <Moon />
+      <Moon ellipsoid={new Cesium.Ellipsoid(radius, radius, radius)} />
     </Viewer>
   ));
 };
