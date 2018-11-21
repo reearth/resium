38370871153c83e839bf7c02e1ea7e59374dc2dd
rot1024 <aayhrot@gmail.com>
2018-11-21 15:23:47 +0900

    add: Model component

---
+++ src/stories/Model.stories.tsx
@@ -0,0 +1,29 @@
+import Cesium from "cesium";
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import Viewer from "../Viewer";
+import Model from "../Model";
+import CameraFlyTo from "../CameraFlyTo";
+
+import glb from "./models/Cesium_Air.glb";
+
+const origin = Cesium.Cartesian3.fromDegrees(-95.0, 40.0, 200000.0);
+const cameraDest = Cesium.Cartesian3.fromDegrees(-95.0, 40.0, 210000);
+const modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(origin);
+
+export default () => {
+  storiesOf("Model", module).add("default", () => (
+    <Viewer full>
+      <CameraFlyTo destination={cameraDest} duration={0} />
+      <Model
+        url={glb}
+        modelMatrix={modelMatrix}
+        minimumPixelSize={128}
+        maximumScale={20000}
+        onReady={action("onReady")}
+      />
+    </Viewer>
+  ));
+};
