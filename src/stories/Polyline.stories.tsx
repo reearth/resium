ff950b5cc566feaf6aab555b570b1f603104bb34
rot1024 <aayhrot@gmail.com>
2018-11-22 19:30:06 +0900

    change: add event props

--- src/stories/Polyline.stories.tsx
+++ src/stories/Polyline.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Cartesian3, Color, Transforms } from "cesium";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 
 import Viewer from "../Viewer";
 import Polyline from "../Polyline";
@@ -14,12 +15,41 @@ const positions = [
   new Cartesian3(-125, 135, 0),
 ];
 export default () => {
-  storiesOf("Polyline", module).add("default", () => (
-    <Viewer full>
-      <PolylineCollection modelMatrix={Transforms.eastNorthUpToFixedFrame(center)}>
-        <Polyline positions={positions} width={10} />
-      </PolylineCollection>
-      <CameraFlyTo duration={0} destination={Cartesian3.fromDegrees(-75.6, 40.04, 1000)} />
-    </Viewer>
-  ));
+  storiesOf("Polyline", module)
+    .add("default", () => (
+      <Viewer full>
+        <PolylineCollection modelMatrix={Transforms.eastNorthUpToFixedFrame(center)}>
+          <Polyline positions={positions} width={10} />
+        </PolylineCollection>
+        <CameraFlyTo duration={0} destination={Cartesian3.fromDegrees(-75.6, 40.04, 1000)} />
+      </Viewer>
+    ))
+    .add("Events", () => (
+      <Viewer full>
+        <PolylineCollection modelMatrix={Transforms.eastNorthUpToFixedFrame(center)}>
+          <Polyline
+            positions={positions}
+            width={10}
+            onClick={action("onClick")}
+            onDoubleClick={action("onDoubleClick")}
+            onMouseDown={action("onMouseDown")}
+            onMouseUp={action("onMouseUp")}
+            onMiddleClick={action("onMiddleClick")}
+            onMiddleDown={action("onMiddleDown")}
+            onMiddleUp={action("onMiddleUp")}
+            onMouseMove={action("onMouseMove")}
+            onPinchEnd={action("onPinchEnd")}
+            onPinchMove={action("onPinchMove")}
+            onPinchStart={action("onPinchStart")}
+            onRightClick={action("onRightClick")}
+            onRightDown={action("onRightDown")}
+            onRightUp={action("onRightUp")}
+            onWheel={action("onWheel")}
+            onMouseEnter={action("onMouseEnter")}
+            onMouseLeave={action("onMouseLeave")}
+          />
+        </PolylineCollection>
+        <CameraFlyTo duration={0} destination={Cartesian3.fromDegrees(-75.6, 40.04, 1000)} />
+      </Viewer>
+    ));
 };
