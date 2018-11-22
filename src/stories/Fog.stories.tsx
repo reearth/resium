2298cc4ca7151ac762c4f04ee2e9b7f2e82e2c73
Shinnosuke Komiya <kurohune538@gmail.com>
2018-11-22 19:54:06 +0900

    add: add disable scene of Fog

--- src/stories/Fog.stories.tsx
+++ src/stories/Fog.stories.tsx
@@ -5,9 +5,15 @@ import Viewer from "../Viewer";
 import Fog from "../Fog";
 
 export default () => {
-  storiesOf("Fog", module).add("default", () => (
-    <Viewer full>
-      <Fog enabled={false} />
-    </Viewer>
-  ));
+  storiesOf("Fog", module)
+    .add("default", () => (
+      <Viewer full>
+        <Fog />
+      </Viewer>
+    ))
+    .add("disabled", () => (
+      <Viewer full>
+        <Fog enabled={false} />
+      </Viewer>
+    ));
 };
