a61bd4b7591c92a556c8742c8978240f14d6a6b2
Shinnosuke Komiya <kurohune538@gmail.com>
2018-11-26 19:14:46 +0900

    add: add PolylineGraphics component

---
+++ src/PolylineGraphics.ts
@@ -0,0 +1,69 @@
+import Cesium from "cesium";
+
+import createCesiumComponent, { EventkeyMap } from "./core/CesiumComponent";
+
+export interface PolylineGraphicsCesiumProps {
+  positions?: Cesium.Property | Cesium.Cartesian3[];
+  followSurface?: Cesium.Property | boolean;
+  clampToGround?: Cesium.Property | boolean;
+  width?: Cesium.Property | number;
+  show?: Cesium.Property | boolean;
+  material?: Cesium.MaterialProperty | Cesium.Color | string;
+  depthFailMaterial?: Cesium.MaterialProperty | Cesium.Color | string;
+  granularity?: Cesium.Property | number;
+  shadows?: Cesium.Property | Cesium.ShadowMode;
+  distanceDisplayCondition?: Cesium.Property | Cesium.DistanceDisplayCondition;
+  zIndex?: Cesium.Property | number;
+}
+
+export interface PolylineGraphicsProps extends PolylineGraphicsCesiumProps {
+  onDefinitionChange?: () => void;
+}
+
+export interface PolylineGraphicsContext {
+  entity?: Cesium.Entity;
+}
+
+const cesiumProps: Array<keyof PolylineGraphicsCesiumProps> = [
+  "positions",
+  "followSurface",
+  "clampToGround",
+  "width",
+  "show",
+  "material",
+  "depthFailMaterial",
+  "granularity",
+  "shadows",
+  "distanceDisplayCondition",
+  "zIndex",
+];
+
+const cesiumEventProps: EventkeyMap<Cesium.PolylineGraphics, keyof PolylineGraphicsProps> = {
+  definitionChanged: "onDefinitionChange",
+};
+
+const PolylineGraphics = createCesiumComponent<
+  Cesium.PolylineGraphics,
+  PolylineGraphicsProps,
+  PolylineGraphicsContext
+>({
+  name: "PolylineGraphics",
+  create(cprops) {
+    // workaround: type of "image" prop
+    return new Cesium.PolylineGraphics(cprops as any);
+  },
+  mount(element, context) {
+    if (context.entity) {
+      context.entity.polyline = element;
+    }
+  },
+  unmount(element, context) {
+    if (context.entity) {
+      context.entity.polyline = undefined as any;
+    }
+  },
+  cesiumProps,
+  cesiumEventProps,
+});
+
+export default PolylineGraphics;
