b463b40fc3ad0da6add82fe1fbc4736b2fc8d787
Shinnosuke Komiya <kurohune538@gmail.com>
2018-11-26 19:16:29 +0900

    add: add PointGraphics component

---
+++ src/PointGraphics.ts
@@ -0,0 +1,67 @@
+import Cesium from "cesium";
+
+import createCesiumComponent, { EventkeyMap } from "./core/CesiumComponent";
+
+export interface PointGraphicsCesiumProps {
+  color?: Cesium.Property | Cesium.Color;
+  pixelSize?: Cesium.Property | number;
+  outlineColor?: Cesium.Property | Cesium.Color;
+  outlineWidth?: Cesium.Property | number;
+  show?: Cesium.Property | boolean;
+  scaleByDistance?: Cesium.Property | Cesium.NearFarScalar;
+  translucencyByDistance?: Cesium.Property | Cesium.NearFarScalar;
+  heightReference?: Cesium.Property | Cesium.HeightReference;
+  distanceDisplayCondition?: Cesium.Property | Cesium.DistanceDisplayCondition;
+  disableDepthTestDistance?: Cesium.Property | number;
+}
+
+export interface PointGraphicsProps extends PointGraphicsCesiumProps {
+  onDefinitionChange?: () => void;
+}
+
+export interface PointGraphicsContext {
+  entity?: Cesium.Entity;
+}
+
+const cesiumProps: Array<keyof PointGraphicsCesiumProps> = [
+  "color",
+  "pixelSize",
+  "outlineColor",
+  "outlineWidth",
+  "show",
+  "scaleByDistance",
+  "translucencyByDistance",
+  "heightReference",
+  "distanceDisplayCondition",
+  "disableDepthTestDistance",
+];
+
+const cesiumEventProps: EventkeyMap<Cesium.PointGraphics, keyof PointGraphicsProps> = {
+  definitionChanged: "onDefinitionChange",
+};
+
+const PointGraphics = createCesiumComponent<
+  Cesium.PointGraphics,
+  PointGraphicsProps,
+  PointGraphicsContext
+>({
+  name: "PointGraphics",
+  create(cprops) {
+    // workaround: type of "image" prop
+    return new Cesium.PointGraphics(cprops as any);
+  },
+  mount(element, context) {
+    if (context.entity) {
+      context.entity.point = element;
+    }
+  },
+  unmount(element, context) {
+    if (context.entity) {
+      context.entity.point = undefined as any;
+    }
+  },
+  cesiumProps,
+  cesiumEventProps,
+});
+
+export default PointGraphics;
