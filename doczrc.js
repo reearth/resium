50c2228700fab104ffd4b0c54ad8c0bab480be4a
rot1024 <aayhrot@gmail.com>
2018-12-05 17:28:42 +0900

    doc: set up docz

---
+++ doczrc.js
@@ -0,0 +1,43 @@
+import webpack from "webpack";
+import CopyPlugin from "copy-webpack-plugin";
+
+export default {
+  typescript: true,
+  htmlContext: {
+    head: {
+      links: [
+        {
+          rel: "stylesheet",
+          href: "/cesium/Widgets/widgets.css",
+        },
+      ],
+      raw: ["<style>img{max-width:100%;}</style>"],
+    },
+    body: {
+      scripts: [{ src: "/cesium/Cesium.js" }],
+    },
+  },
+  modifyBundlerConfig: config => ({
+    ...config,
+    externals: {
+      ...config.externals,
+      cesium: "Cesium",
+    },
+    plugins: [
+      ...config.plugins,
+      new webpack.DefinePlugin({
+        CESIUM_BASE_URL: JSON.stringify("/cesium"),
+      }),
+      new CopyPlugin([
+        {
+          from: "node_modules/cesium/Build/CesiumUnminified",
+          to: "cesium",
+        },
+      ]),
+    ],
+    resolve: {
+      ...config.resolve,
+      extensions: [...config.resolve.extensions, ".ts", ".tsx"],
+    },
+  }),
+};
