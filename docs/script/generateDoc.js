5e69ce6273bb972c6157a02b2d2fb04d6fb59cef
rot1024 <aayhrot@gmail.com>
2018-12-13 20:31:15 +0900

    doc: implement script to generate api docs

---
+++ docs/script/generateDoc.js
@@ -0,0 +1,214 @@
+"use strict";
+const fs = require("fs");
+const path = require("path");
+const ts = require("typescript");
+
+function renderPropTable(types) {
+  if (!types || types.length === 0) return "N/A";
+
+  return `
+| Property | Type | Description |
+|--|--|--|
+${types
+    .map(t => {
+      const type = t.type
+        .replace(
+          /Cesium\.(.+?)( |<|>|,|\[|\)|$)/g,
+          "[Cesium.$1](https://cesiumjs.org/Cesium/Build/Documentation/$1.html)$2",
+        )
+        .replace(/\|/g, "&#124;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .split("\n")
+        .map(s => s.trim())
+        .join(" ");
+      return `| ${t.name} | ${type} | ${t.description || ""} |`;
+    })
+    .join("\n")}
+`.trim();
+}
+
+function type2doc(type) {
+  return `
+---
+name: ${type.name}
+route: /components/${type.name}
+menu: Components
+---
+
+${
+    type.playground
+      ? `
+import { Playground } from "docz";
+import Viewer from "../components/Viewer";
+`.trim()
+      : ""
+  }
+
+# ${type.name}
+
+${type.summary || ""}
+
+**Cesium element**: [${type.name}](https://cesiumjs.org/Cesium/Build/Documentation/${
+    type.name
+  }.html)
+
+${
+    type.example
+      ? `
+<Playground>
+${type.example
+          .split("\n")
+          .map(s => "  " + s)
+          .join("\n")}
+</Playground>
+`.trim()
+      : ""
+  }
+
+
+${
+    type.scope
+      ? `
+## Available scope
+
+${type.scope}
+`.trim()
+      : ""
+  }
+
+## Properties
+
+### Cesium properties
+
+${renderPropTable(type.cesiumProps)}
+
+### Cesium read only properties
+
+${renderPropTable(type.cesiumReadonlyProps)}
+
+### Cesium events
+
+${renderPropTable(type.cesiumEvents)}
+
+### Other properties
+
+${renderPropTable(type.props)}
+`.trim();
+}
+
+function getComment(node, source) {
+  const comments = ts.getTrailingCommentRanges(source, node.getEnd());
+  if (comments && comments[0]) {
+    return source.slice(comments[0].pos, comments[0].end);
+  }
+  return undefined;
+}
+
+function getProp(node, source) {
+  const comment = getComment(node, source);
+  const formattedComment = comment
+    ? comment
+        .replace("// ", "")
+        .replace(";", "")
+        .trim()
+    : undefined;
+
+  let optional = false;
+  let counter = 0;
+  let type = "";
+  node.forEachChild(node2 => {
+    if (node2.kind === ts.SyntaxKind.QuestionToken) {
+      optional = true;
+    } else if (counter === 1 || (optional && counter == 2)) {
+      // this is type
+      type = node2.getText();
+    }
+    counter++;
+  });
+
+  const formattedType = type.replace(/:.+?\/\* (.+) \*\/(,|\))/g, ": $1$2");
+
+  return {
+    name: node.name.escapedText,
+    type: formattedComment || formattedType,
+    required: !optional,
+  };
+}
+
+function parsePropTypes(name, source) {
+  const sourceFile = ts.createSourceFile(name + ".ts", source, ts.ScriptTarget.ES6, true);
+  const props = {
+    name,
+    cesiumProps: [],
+    cesiumReadonlyProps: [],
+    cesiumEvents: [],
+    props: [],
+  };
+  const eventMap = [];
+
+  sourceFile.forEachChild(node => {
+    if (node.kind === ts.SyntaxKind.InterfaceDeclaration) {
+      const name = node.name.escapedText;
+      const key = /CesiumProps$/.test(name)
+        ? "cesiumProps"
+        : /CesiumReadonlyProps$/.test(name)
+        ? "cesiumReadonlyProps"
+        : /CesiumEvents$/.test(name)
+        ? "cesiumEvents"
+        : /Props$/.test(name)
+        ? "props"
+        : undefined;
+      if (!key) return;
+
+      node.forEachChild(node2 => {
+        if (node2.kind === ts.SyntaxKind.PropertySignature) {
+          const p = getProp(node2, source);
+          if (/^on[A-Z]/.test(p.name)) {
+            props.cesiumEvents.push(p);
+          } else {
+            props[key].push(p);
+          }
+        }
+      });
+    } else if (
+      node.kind === ts.SyntaxKind.VariableStatement &&
+      node.declarationList.declarations[0] &&
+      node.declarationList.declarations[0].name.escapedText === "cesiumEventProps"
+    ) {
+      node.declarationList.declarations[0].initializer.forEachChild(node2 => {
+        if (
+          node2.kind !== ts.SyntaxKind.PropertyAssignment ||
+          !node2.initializer ||
+          !node2.initializer.text
+        )
+          return;
+
+        eventMap.push([node2.initializer.text, node2.name.escapedText]);
+      });
+    }
+  });
+
+  eventMap.forEach(ev => {
+    const ev2 = props.cesiumEvents.find(e => e.name === ev[0]);
+    if (ev2) {
+      ev2.description = `Correspond to [${name}#${
+        ev[1]
+      }](https://cesiumjs.org/Cesium/Build/Documentation/${name}.html#${ev[1]})`;
+    }
+  });
+
+  return props;
+}
+
+const componentFiles = fs
+  .readdirSync(path.resolve(__dirname, "..", "..", "src"))
+  .filter(cf => /\.ts$/.test(cf) && !/index\.ts$/.test(cf));
+
+componentFiles.forEach(cf => {
+  const name = cf.replace(".ts", "");
+  const code = fs.readFileSync(path.resolve(__dirname, "..", "..", "src", cf), "utf8");
+  const props = parsePropTypes(name, code);
+  const result = type2doc(props);
+  fs.writeFileSync(path.resolve(__dirname, "..", "..", "docs", "api", `${name}.mdx`), result);
+});
