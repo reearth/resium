14b1cc834d1edeb1c87d198dfa0e6973e6e8c5ae
rot1024 <aayhrot@gmail.com>
2019-05-07 15:09:22 +0900

    chore: set cesium ion access token for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,9 @@
+import Cesium from "cesium";
 import { configure, addParameters } from "@storybook/react";
 import { create } from "@storybook/theming";
+
+Cesium.Ion.defaultAccessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjMmMxODRjYy1mYzFiLTQ5MTUtODE1MS02NGNkMzAyNTIyODciLCJpZCI6MTA2OTgsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJhc3NldHMiOlsyLDMsNCwxXSwiaWF0IjoxNTU3MjA1NTM1fQ.5TYPEJKj_JzGX4r_a6GQjwSu7TIW2BIzeaIW8gFLUec";
 
 addParameters({
   options: {
