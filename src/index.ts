593ede6b6b141992d9237f293d42da0b45628f1b
rot1024 <aayhrot@gmail.com>
2019-08-07 16:11:41 +0900

    expose GroundPrimitive and GroundPrimitiveCollection

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,8 @@ export { default as EntityDescription } from "./EntityDescription";
 export { default as Fog } from "./Fog";
 export { default as GeoJsonDataSource } from "./GeoJsonDataSource";
 export { default as Globe } from "./Globe";
+export { default as GroundPrimitive } from "./GroundPrimitive";
+export { default as GroundPrimitiveCollection } from "./GroundPrimitiveCollection";
 export { default as ImageryLayer } from "./ImageryLayer";
 export { default as ImageryLayerCollection } from "./ImageryLayerCollection";
 export { default as KmlDataSource } from "./KmlDataSource";
