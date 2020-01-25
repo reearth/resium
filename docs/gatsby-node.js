09716bf2ed54d39de96cc942e201293bcea14794
lightnet328 <lightnet328@gmail.com>
2020-01-26 01:22:35 +0900

    fix: build storybook

---
+++ docs/gatsby-node.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+  const config = getConfig()
+  actions.replaceWebpackConfig({
+    ...config,
+    // for storybook
+    externals: { cesium: "Cesium" },
+    // for storybook
+    plugins: [
+      ...config.plugins,
+      new webpack.DefinePlugin({
+        CESIUM_BASE_URL: JSON.stringify("/cesium"),
+      }),
+      new CopyPlugin([
+        {
+          from: path.resolve(__dirname, "../node_modules/cesium/Build/Cesium"),
+          to: path.resolve(__dirname, "dist/cesium"),
+        },
+      ]),
+    ],
+    resolve: {
+      ...config.resolve,
+      extensions: [...config.resolve.extensions, ".ts", ".tsx"],
+    },
+  })
+}
