01941bf4daee7294445bdb016c14209d46ebf949
Shinnosuke Komiya <kurohune538@gmail.com>
2020-02-06 15:13:01 +0900

    feat: add ga plugin

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-132064508-1",
+      },
+    },
+  ],
+};
