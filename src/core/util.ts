25cb707856bd48ac004d573b1845e08459463be3
benwiles1 <wiles@metsci.com>
2020-04-30 08:17:30 -0400

    fix: mouse moves on primitives (#324)
    Co-Authored-By: rot1024 <rot1024@gmail.com>

--- src/core/util.ts
+++ src/core/util.ts
@@ -20,10 +20,7 @@ export const pickedObjectEquals = (picked: any, element: any) =>
   !!picked &&
   (picked === element ||
     (!!picked.id && picked.id === element) ||
-    (!!picked.primitive &&
-      (picked.primitive === element ||
-        (!!picked.primitive.equals && picked.primitive.equals(element)) ||
-        polylineEquals(picked.primitive, element))));
+    picked.equals?.(element) || polylineEquals(picked, element));
 
 export function pick<T, K extends keyof T>(obj: T, keys?: K[]): Pick<T, K> {
   if (!keys) return {} as Pick<T, K>;
