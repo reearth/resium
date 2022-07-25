da324b0b348f99c97662a98ee8aa6963660638f0
rot1024 <aayhrot@gmail.com>
2022-07-25 13:42:58 +0900

    fix: build config

--- vite.config.ts
+++ vite.config.ts
@@ -6,39 +6,38 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import { configDefaults } from "vitest/config";
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins:
-      mode === "test"
-        ? [react()]
-        : [
-            dts({
-              rollupTypes: true,
-              exclude: ["./src/env.d.ts"],
-            }),
-          ],
-    build: {
-      lib: {
-        entry: "src/index.ts",
-        name: "Resium",
-      },
-      rollupOptions: {
-        external: ["cesium", "react"],
-        output: {
-          globals: {
-            cesium: "Cesium",
-            react: "React",
-          },
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react({
+      jsxRuntime: mode === "production" ? "classic" : "automatic",
+    }),
+    dts({
+      rollupTypes: true,
+      exclude: ["./src/env.d.ts"],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "Resium",
+    },
+    rollupOptions: {
+      external: ["cesium", "react", "react-dom"],
+      output: {
+        globals: {
+          cesium: "Cesium",
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
-    test: {
-      environment: "jsdom",
-      exclude: [...configDefaults.exclude, "docs/**/*", "examples/**/*"],
-      setupFiles: ["src/test/setup.ts"],
-      coverage: {
-        reporter: ["text", "json"],
-      },
+  },
+  test: {
+    environment: "jsdom",
+    exclude: [...configDefaults.exclude, "docs/**/*", "examples/**/*"],
+    setupFiles: ["src/test/setup.ts"],
+    coverage: {
+      reporter: ["text", "json"],
     },
-  };
-});
+  },
+}));
