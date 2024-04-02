d193b82a9bb299e9c524aff16574ff459d937a6c
keiya01 <keiya.s.0210@gmail.com>
2024-04-02 11:25:05 +0900

    fix

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-namespace */
-import matchers, { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
+import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
+import * as matchers from "@testing-library/jest-dom/matchers";
 import { expect } from "vitest";
 
 // Vitest on GitHub Actions requires TransformStream to run tests with Cesium
-import "web-streams-polyfill/es2018";
+import "web-streams-polyfill/polyfill";
 
 declare global {
   namespace Vi {
