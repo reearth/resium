8a77e8e015a99402e2c77acd7b91f2d1bf221f1e
keiya01 <34934510+keiya01@users.noreply.github.com>
2024-08-20 12:27:00 +0900

    fix: prevent unnecessary re-mount

--- src/Globe/Globe.stories.tsx
+++ src/Globe/Globe.stories.tsx
@@ -1,11 +1,11 @@
 import { action } from "@storybook/addon-actions";
 import { Meta, StoryObj } from "@storybook/react";
 import { CesiumTerrainProvider, EllipsoidTerrainProvider, IonResource } from "cesium";
+import { StrictMode } from "react";
 
 import Viewer from "../Viewer";
 
 import Globe from "./Globe";
-import { StrictMode } from "react";
 
 type Story = StoryObj<typeof Globe>;
 
@@ -28,16 +28,20 @@ export const Basic: Story = {
 };
 
 export const Promise: Story = {
-  args: { enableLighting: true },
-  render: args => (
+  args: { enableLighting: true, isTerrainEnabled: false } as any,
+  render: ({ isTerrainEnabled, ...args }: any) => (
     <StrictMode>
       <Viewer full>
         <Globe
           {...args}
-          terrainProvider={CesiumTerrainProvider.fromUrl(IonResource.fromAssetId(1), {
-            requestVertexNormals: true,
-            requestWaterMask: false,
-          })}
+          terrainProvider={
+            isTerrainEnabled
+              ? CesiumTerrainProvider.fromUrl(IonResource.fromAssetId(1), {
+                  requestVertexNormals: true,
+                  requestWaterMask: false,
+                })
+              : new EllipsoidTerrainProvider()
+          }
           onImageryLayersUpdate={action("onImageryLayersUpdate")}
           onTerrainProviderChange={action("onTerrainProviderChange")}
         />
