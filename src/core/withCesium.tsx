d7b9ed754d0aaedbcef66a13651bb064b2f0d975
rot1024 <aayhrot@gmail.com>
2025-02-07 10:19:41 +0900

    fix type error

--- src/core/withCesium.tsx
+++ src/core/withCesium.tsx
@@ -25,7 +25,9 @@ export type CesiumHOCComponentType<E, P> = ForwardRefExoticComponent<
 export type WithContextProps<P, C> = P & CesiumProp<C>;
 export type WithContextType<P, C> = ComponentType<WithContextProps<P, C>>;
 
-export const withCesium = <P, C>(Component: WithContextType<P, C>) =>
+export const withCesium = <P, C>(
+  Component: WithContextType<PropsWithoutRef<P> & { cesium?: any }, C>,
+) =>
   // supports both functional components and class components
   // eslint-disable-next-line react/display-name
   forwardRef<WithContextType<P, C>, P>((props, ref) => (
