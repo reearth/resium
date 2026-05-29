fe1e2855fa52b17812af117f8d74f182139c34e6
rot1024 <aayhrot@gmail.com>
2026-05-30 03:09:42 +0900

    fix(Clock): bind timeline to the clock range in example (#620)

--- src/Clock/Clock.stories.tsx
+++ src/Clock/Clock.stories.tsx
@@ -1,6 +1,8 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { JulianDate, ClockRange, ClockStep } from "cesium";
+import { JulianDate, ClockRange, ClockStep, Viewer as CesiumViewer } from "cesium";
+import { useEffect, useMemo, useRef } from "react";
 
+import { CesiumComponentRef } from "../core";
 import Globe from "../Globe";
 import Viewer from "../Viewer";
 
@@ -14,18 +16,33 @@ export default {
 } as Meta;
 
 export const Basic: Story = {
-  render: () => (
-    <Viewer full>
-      <Globe enableLighting />
-      <Clock
-        startTime={JulianDate.fromIso8601("2013-12-25")}
-        currentTime={JulianDate.fromIso8601("2013-12-25")}
-        stopTime={JulianDate.fromIso8601("2013-12-26")}
-        clockRange={ClockRange.LOOP_STOP} // loop when we hit the end time
-        clockStep={ClockStep.SYSTEM_CLOCK_MULTIPLIER}
-        multiplier={4000} // how much time to advance each tick
-        shouldAnimate // Animation on by default
-      />
-    </Viewer>
-  ),
+  render: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const ref = useRef<CesiumComponentRef<CesiumViewer>>(null);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const startTime = useMemo(() => JulianDate.fromIso8601("2013-12-25"), []);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const stopTime = useMemo(() => JulianDate.fromIso8601("2013-12-26"), []);
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+      // Zoom the bottom Timeline widget to the clock's time range so it is bound to the clock.
+      ref.current?.cesiumElement?.timeline?.zoomTo(startTime, stopTime);
+    }, [startTime, stopTime]);
+
+    return (
+      <Viewer full ref={ref}>
+        <Globe enableLighting />
+        <Clock
+          startTime={startTime}
+          currentTime={startTime}
+          stopTime={stopTime}
+          clockRange={ClockRange.LOOP_STOP} // loop when we hit the end time
+          clockStep={ClockStep.SYSTEM_CLOCK_MULTIPLIER}
+          multiplier={4000} // how much time to advance each tick
+          shouldAnimate // Animation on by default
+        />
+      </Viewer>
+    );
+  },
 };
